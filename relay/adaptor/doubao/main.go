73797c637aa26fcfe5869baeff91d98edca454a1
1196239739@qq.com <1196239739@qq.com>
2025-03-19 14:39:08 +0800

    feat:Enhance VolcEngine channel support with bot model routing

--- relay/adaptor/doubao/main.go
+++ relay/adaptor/doubao/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"github.com/songquanpeng/one-api/relay/meta"
 	"github.com/songquanpeng/one-api/relay/relaymode"
+	"strings"
 )
 
 func GetRequestURL(meta *meta.Meta) (string, error) {
 	switch meta.Mode {
 	case relaymode.ChatCompletions:
+		if strings.HasPrefix(meta.ActualModelName, "bot") {
+			return fmt.Sprintf("%s/api/v3/bots/chat/completions", meta.BaseURL), nil
+		}
 		return fmt.Sprintf("%s/api/v3/chat/completions", meta.BaseURL), nil
 	case relaymode.Embeddings:
 		return fmt.Sprintf("%s/api/v3/embeddings", meta.BaseURL), nil
